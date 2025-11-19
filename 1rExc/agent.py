f13f4b0d05deb4184bd3561993403cec118329e5
Marti <lazarobaguemarti@gmail.com>
2025-11-19 10:35:25 +0100

    creant Agent

--- 1rExc/agent.py
+++ 1rExc/agent.py
@@ -10,26 +10,43 @@ class Agent:
     # Cada quant explirem (fem coses rares, bogeries, coses que no feiem abans)
     epsilon=0.9
 
+    actions=('up', 'down', 'right', 'left')
 
-    def policy():
+
+    def policy(self):
         """
         Mètode que executarà l'acció. Epsilon greedy?
         """
         if random.random() < self.epsilon:
             # Exploració
-            return acció_aleatòria()
+            return self.explore()
         else:
             # Explotació
-            return acció_amb_max_Q(state)
+            return self.max_Q()
 
         return "a"
 
-    def learn():
+    def learn(self):
         """
         Mètode que apendra de l'acció feta. Bellman
         """
                
         return "b"
+    
+    def explore(self):
+        rand=random.random()
+
+        if rand < 0.25:
+            return self.actions[0]
+        elif rand < 0.5:
+            return self.actions[1]
+        elif rand < 0.75:
+            return self.actions[2]
+        else:
+            return self.actions[3]
+        
+    def max_Q(self):
+        return 'up'
 
 
 
