ed63cead2f27eb7c7ed401aa871044612fa10077
MartiVila <martivilarebellon@gmail.com>
2025-12-13 15:24:16 +0100

    convergence working properly

--- 2nExc/main_convergence.py
+++ 2nExc/main_convergence.py
@@ -2,131 +2,67 @@ import agent
 import aichess 
 import matplotlib.pyplot as plt
 import numpy as np
-def plot_convergence(rewards_history, iterations,conv_point, filename='convergence_plot.png'):
+
+def plot_convergence(history_to_plot, iterations,conv_point, filename='convergence_plot.png'):
     """
-    Genera un gràfic de la convergència (recompensa per episodi) i el guarda en un fitxer.
-    
-    Args:
-        rewards_history (list): Llista amb la recompensa total de cada episodi.
-        window_size (int): Mida de la finestra per calcular la mitjana mòbil.
-        filename (str): Nom del fitxer on es guardarà la imatge.
+    Creats the plot for convergence based on the difference in Q'table values (loss history).
     """
     plt.figure(figsize=(10, 6))
     
-    # Gràfic de les recompenses brutes (en blau clar i transparent per veure la variància)
-    plt.plot(rewards_history, color='red', alpha=0.6, label='Convergencia per episodi')
-    
-    """    # Càlcul i gràfic de la mitjana mòbil per veure la tendència (en blau fosc)
-    if len(rewards_history) >= iterations:
-        moving_average = np.convolve(rewards_history, np.ones(iterations)/iterations, mode='valid')
-        # Ajustem l'eix X perquè la mitjana coincideixi amb el final de la finestra
-        plt.plot(range(iterations-1, len(rewards_history)), moving_average, color='blue', linewidth=2, label=f'Mitjana mòbil ({iterations})')
-    """
-    
-    plt.axvline(x=conv_point, color='blue', linestyle='--', linewidth=2, label=f'Marca punt on es troba la convergencia (x={conv_point})')
-    plt.title("Convergència de l'Agent: Convergencia vs Episodis")
-    plt.xlabel("Episodis")
-    plt.ylabel("Convergencia")
+    #evolution of the diffrence in Q-table values
+    plt.plot(history_to_plot, color='red', alpha=0.6, label='difference in Q-table values')
+
+    plt.axvline(x=conv_point, color='blue', linestyle='--', linewidth=2, label=f'Point of convergence (x={conv_point})')
+    plt.title("Agent convergence through episodes")
+    plt.xlabel("Episodes")
+    plt.ylabel("Convergence")
     plt.legend()
     plt.grid(True, alpha=0.3)
-    
-    # Guardar el gràfic
+
     plt.savefig(filename)
-    print(f"Gràfic guardat a {filename}")
-    plt.show() # Descomenta això si l'executes en local i vols veure la finestra
-
-def check_convergence(rewards, min_episodes=50):
-    """
-    Calcula la convergència.
-    min_episodes: Nombre mínim d'episodis abans de comprovar si s'ha estancat.
-    """
-    if len(rewards) == 0:
-        return 0.0, -1
-    
-    iterations = -1
-    conv_list=[]
-    # Comencem a 'min_episodes' o a 1, el que sigui més gran.
-    start_index = 1 if min_episodes < 50 else min_episodes
-    avg_best = 0.0
-    
-    for i in range(len(rewards)-1):
-        if i > start_index and avg_best == 0.0:
-            if (rewards[i-1]<0 or rewards[i]<0 or rewards[i+1]<0):
-                continue  # Ignorem estancaments en recompenses negatives
-            
-
-            # Comprovem si hi ha 3 valors idèntics consecutius
-            variation1 = rewards[i-1] - rewards[i]
-            variation2 = rewards[i] - rewards[i+1]
-
-            if -20 <= variation1 <= 20 and -20 <= variation2 <= 20:
-                # Només considerem estancament si el reward és raonable   
-                if rewards[i] > 0: 
-                    avg_best = sum(rewards[i-2:i+2]) / i
-                    conv_point = i
-                
-        
-        if i >= 2:
-            conv_list.append(abs(sum(rewards[i-2:i+2]) / i))
-            
-    # Si no troba convergència, retornem el total d'episodis com a "temps de convergència"
-    if iterations == -1:
-        iterations = len(rewards)
-            
-    return avg_best, conv_point, conv_list
-
+    print(f"saved as {filename}")
+    plt.show()
 
 def check_convergence_final(loss_history, rewards, min_episodes=50, loss_thresh=5.0, reward_std_thresh=80.0):
     """
-    Calcula la convergència.
-    min_episodes: Nombre mínim d'episodis abans de comprovar si s'ha estancat.
+    Calculates convergence based solely on the Q-table values (Loss).
+    min_episodes: Minimum number of episodes before checking for stagnation.
     """
-    if len(rewards) == 0:
-        return 0.0, -1
+    if len(loss_history) == 0:
+        return 0.0, -1, []
     
     iterations = -1
-    conv_list=[]
-    # Comencem a 'min_episodes' o a 1, el que sigui més gran.
+    #conv_list stores the loss history for plotting
+    conv_list=[] 
     start_index = 1 if min_episodes < 50 else min_episodes
     avg_best = 0.0
     conv_point=None
     
-    for i in range(len(rewards)-1):
+    #we iterate respecting the number of q-tables
+    for i in range(len(loss_history)): 
         
-        recent_reward = rewards[:i]
-        std_reward = np.std(recent_reward)
-
-        if i > start_index and avg_best == 0.0:
-
-            if (rewards[i-1]<0 or rewards[i]<0):
-                conv_list.append(std_reward) 
-                continue  # Ignorem estancaments en recompenses negatives
-            
-            recent_reward = rewards[(i-50):i]
-            std_reward = np.std(recent_reward)
-            recent_loss = loss_history[(i-50):i]
-            avg_loss = np.mean(recent_loss)
-
-
-            # A. El cervell està estable (Loss baix)
-            is_brain_stable = avg_loss < loss_thresh
-            
-            # B. Els resultats són estables (Poca variació en els punts)
-            # Nota: No mirem si el reward és alt, sinó si és ESTABLE.
-            is_performance_stable = std_reward < reward_std_thresh
-
-            if is_brain_stable and is_performance_stable:
-                # Només considerem estancament si el reward és raonable   
-                if rewards[i] > 0: 
-                    start_window = max(0, i - min_episodes)
-                    avg_best = np.mean(rewards[start_window:i]) 
-                    conv_point = i
-                
+        if i < min_episodes:
+            conv_list.append(loss_history[i])
+            continue
         
+        #calculate the average loss over the recent 50 episodes
+        recent_loss = loss_history[(i-50):i]
+        avg_loss = np.mean(recent_loss)
         
-        conv_list.append(std_reward)  
+        # A. The brain is stable (Low Loss)
+        is_brain_stable = avg_loss < loss_thresh
+        
+        #average loss
+        conv_list.append(avg_loss)
+
+        if conv_point is None: #we keep l;ooking until found the ocnvergeence pioint
+            if is_brain_stable:
+                start_window = max(0, i - min_episodes)
+                avg_best = np.mean(rewards[start_window:i]) 
+                conv_point = i
+        
             
-    # Si no troba convergència, retornem el total d'episodis com a "temps de convergència"
+    #Case we have not found any convergence point, we return 0.0 and -1
     if conv_point is None:
         return 0.0, -1, conv_list
             
@@ -134,20 +70,20 @@ def check_convergence_final(loss_history, rewards, min_episodes=50, loss_thresh=
 
 
 
-def Convergence_sim(self, agent, num_episodes, max_steps_per_episode=200, reward_func='simple', stochasticity=0.0):
-        #This function is the core of the algorithm that trains the agent using Q-learning
+def Convergence_sim(self, agent, num_episodes, max_steps_per_episode=200, reward_func='heuristic', stochasticity=0.0):
+        """
+        Simulatate the agent q-learning to check convergence.
+        It's like Q-learning with minor changes to be able to store results and find the point whre the Q-table stabilizes.
+        """
+
 
         initial_state = self.chess.getCurrentState() 
         initial_state_key = self.state_to_key(initial_state)
         rewards_history = []
-        Qtables_history = []
-
-        current_delta_loses = []
+        loss_history = []
 
         for episode in range(num_episodes):
-            #This part is just to capture the Qtables.
-         
-
+            
             self.chess.reset_environment()
             current_state = self.chess.getCurrentState()
             self.listVisitedStates = []
@@ -155,6 +91,8 @@ def Convergence_sim(self, agent, num_episodes, max_steps_per_episode=200, reward
             total_reward = 0
             
             state_key = self.state_to_key(current_state)
+
+            current_episode_losses = []
 
             for step in range(max_steps_per_episode):
                 legal_next_states = self.chess.get_all_next_states(current_state, True)
@@ -208,7 +146,7 @@ def Convergence_sim(self, agent, num_episodes, max_steps_per_episode=200, reward
                 next_state_key = self.state_to_key(final_next_state)
                 
                 #make the agent learn from the transition
-                current_delta_loses.append(agent.learn(state_key, action_index, reward, next_state_key, done, num_actions))
+                current_episode_losses.append(agent.learn(state_key, action_index, reward, next_state_key, done, num_actions))
                 
                 #We actually move the piece in the board
                 self.chess.movePiece(current_state, final_next_state)
@@ -220,7 +158,8 @@ def Convergence_sim(self, agent, num_episodes, max_steps_per_episode=200, reward
                     break
             
             rewards_history.append(total_reward)
-            Qtables_history.append(np.sum(current_delta_loses)/len(current_delta_loses) if current_delta_loses else 0.0)
+            loss_history.append(np.sum(current_episode_losses)/len(current_episode_losses) if current_episode_losses else 0.0)
+            
             #search for the optimal path found so far
             if total_reward > self.best_reward or (total_reward == self.best_reward and step + 1 < self.min_steps_for_best_reward):
                 
@@ -234,32 +173,24 @@ def Convergence_sim(self, agent, num_episodes, max_steps_per_episode=200, reward
             if episode % 1000 == 0: 
                 agent.reduce_exploration_rate_by_30_percent() 
         
-        return Qtables_history, rewards_history
+        return loss_history, rewards_history
 
-
-# --- Main Configuration ---
-"""
-alphas = [0.1, 0.3, 0.5, 0.7]
-gammas = [0.5, 0.7, 0.9]
-epsilons = [0.1, 0.3, 0.5, 0.9]
-"""
 
 alphas = [0.3, 0.5, 0.7]
-gammas = [0.5, 0.7]
-epsilons = [0.3, 0.5]
+gammas = [0.5, 0.7, 0.9]
+epsilons = [0.3, 0.5, 0.9]
 
 def main():
     convergences = []
     best_conv = float("-inf")
     best_alpha = best_gamma = best_epsilon = None
     best_reward_for_best_conv = 0.0
-    best_itr = 3000 # Assumim el màxim inicialment
+    best_itr = 3000
     best_conv_list = []
     
-    # Guardarem el millor agent per demostrar el guardat JSON al final
     best_agent_instance = None 
 
-    print("Iniciant simulació de convergència...")
+    print("Convergence simulation...")
     
     total_iterations = len(alphas) * len(gammas) * len(epsilons)
     current_iter = 0
@@ -268,72 +199,61 @@ def main():
         for gamma in gammas:
             for epsilon in epsilons:
                 current_iter += 1
-                print(f"Simulació {current_iter}/{total_iterations}: alpha={alpha}, gamma={gamma}, epsilon={epsilon}")
+                #Keep trak during all the simulations
+                print(f"simulation {current_iter}/{total_iterations}: alpha={alpha}, gamma={gamma}, epsilon={epsilon}")
                 
-                # Inicialitzem entorn i agent
-                # Utilitzem Aichess ja que conté el Board i lògica auxiliar
+                #Initialize environment and agent
+                #Using Aichess as it contains the Board and auxiliary logic
                 game = aichess.Aichess() 
+                agent_instance = agent.Agent(learning_rate=alpha, future_weight=gamma,exploration_rate=epsilon)
                 
-                agent_instance = agent.Agent(
-                    learning_rate=alpha,
-                    future_weight=gamma,
-                    exploration_rate=epsilon
-                )
+                num_episodes = 2000
+
+                #Here is where the convergence is calculated it self, if any params needed for change HERE
+                loss_history, rewards = Convergence_sim(game, agent_instance, num_episodes=num_episodes, reward_func='heuristic')
+                conv, conv_point, conv_list = check_convergence_final(loss_history, rewards)
                 
-                NUM_EPISODES =3000
-                #rewards = training_simulation(game, agent_instance, num_episodes=NUM_EPISODES, reward_func='heuristic')
-                Qtables, rewards = Convergence_sim(game, agent_instance, num_episodes=NUM_EPISODES, reward_func='heuristic')
-                # Comprovem convergència
-                conv, conv_point, conv_list = check_convergence_final(Qtables, rewards)
-                
-                # Si iterations és -1, significa que no ha trobat plateau, posem el màxim
-                conv_iteration = conv_point if conv_point != -1 else NUM_EPISODES
+                #If there is a -1, the simulation hasn't reached the stability in Q differences, so we set the maximum
+                conv_iteration = conv_point if conv_point != -1 else num_episodes
                 
                 current_best_reward = max(rewards) if len(rewards) > 0 else 0.0
-
                 convergences.append((alpha, gamma, epsilon, conv, current_best_reward, conv_iteration))
                 
-                
-
-                # Criteri: Major convergència (reward mig final) i menor nombre d'iteracions per estabilitzar-se
+                #we a deciding based on two factors, quality and speed, conv is quality, conv_iteration is speed
                 if conv >= best_conv:
-                    # Si la convergència és millor, o és igual però amb menys iteracions (més ràpid)
+                                       
+                    #if quality is strictly better (conv > best_conv),
+                    #if quality is equal but speed is faster (conv_iteration < best_itr).
                     if conv > best_conv or (conv == best_conv and conv_iteration < best_itr):
-                        best_conv = conv
-                        best_alpha, best_gamma, best_epsilon = alpha, gamma, epsilon
-                        best_reward_for_best_conv = current_best_reward
-                        best_itr = conv_iteration
-                        best_agent_instance = agent_instance
-                        best_conv_list = conv_list
+                        best_conv = conv                            
+                        best_alpha, best_gamma, best_epsilon = alpha, gamma, epsilon #best parameters
+                        best_reward_for_best_conv = current_best_reward #maximum reward achieved
+                        best_itr = conv_iteration #best convergence speed, number of episodes
+                        best_conv_list = conv_list 
 
     print("\n" + "="*50)
-    print("Simulació completada.")
-    print("Totes les convergències:")
+    print("convergences")
     for alpha, gamma, epsilon, conv, rew, itr in convergences:
         print(f"alpha={alpha}, gamma={gamma}, epsilon={epsilon} ->max_reward={rew}, conv={conv:.3f}, itr={itr}")
 
-    print("\nMillor convergència trobada:")
+    print("\nbest convergence:")
     print(
         f"alpha={best_alpha}, gamma={best_gamma}, epsilon={best_epsilon} "
-        f"-> convergència={best_conv:.3f}, max_reward={best_reward_for_best_conv}, iteracions={best_itr}"
+        f"-> convergence={best_conv:.3f}, max_reward={best_reward_for_best_conv}, iteracions={best_itr}"
     )
 
-    # Generem gràfic de la millor convergència
-    print("\nGenerant gràfic de la millor convergència...")
+    print("\nPlotting best convergence...")
     print(best_itr)
     print(len(best_conv_list))
     plot_convergence(best_conv_list, iterations=len(best_conv_list), conv_point=best_itr,filename='best_convergence_plot.png')
-    
-    # Demostració de guardar/carregar
+
     if best_agent_instance:
-        print("\nGuardant la Q-table del millor agent...")
+        print("\nsaving Q-table of the best agent")
         agent_instance.save_qtable_to_json("best_agent_qtable.json")
-        
-        # Test de càrrega
-        print("Verificant càrrega...")
+
         dummy_agent = agent.Agent()
         dummy_agent.load_qtable_from_json("best_agent_qtable.json")
-        print(f"Mida de la Q-table carregada: {len(dummy_agent.q_table)} estats.")
+        print(f"Size of loaded Q-table: {len(dummy_agent.q_table)} states.")
 
 if __name__ == "__main__":
     main()
